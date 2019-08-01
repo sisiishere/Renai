24a733faf84e4133c0c7aaabc083bf1ae902db3b
Richard Rattan <16rrattan@gmail.com>
2019-08-01 11:15:23 -0400

    connection.js change

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,16 @@
-var mysql = require("mysql");
-module.exports = function (mysql) {
-    var connection = mysql.createConnection({
-        host: "localhost",
-        port: 8080,
-        user: "root",
-        password: "",
-        database: "haiku_db"
-    })
-}
+var Sequelize = require("sequelize");
+
+// Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
+var sequelize = new Sequelize("haiku_db", "root", "", {
+    host: "localhost",
+    port: 3306,
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    }
+});
+
+// Exports the connection for other files to use
+module.exports = sequelize;

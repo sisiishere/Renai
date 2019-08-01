e14b22ffc5ca46a0619f16b22ca4b71e9f86319f
Kevin Spies <kevin.spies@gmail.com>
2019-08-01 11:42:35 -0400

    api all route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,26 @@
-var db = require("../models");
+var db = require("../models/haiku.js");
 
-module.exports = function(app) {
+module.exports = function (app) {
+
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  app.get("/api/all", function (req, res) {
+    Haiku.findAll({}).then(function (results) {
+      res.json(results);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+  app.post("/api/examples", function (req, res) {
+    db.Example.create(req.body).then(function (dbExample) {
       res.json(dbExample);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.delete("/api/examples/:id", function (req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
       res.json(dbExample);
     });
   });
+
 };
